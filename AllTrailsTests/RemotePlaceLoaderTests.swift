8d871b71c4ad3146614d1c354bf2cbcb6af6a3f8
Jose <daelhoser@gmail.com>
2021-11-10 14:20:53 -0800

    Returns Error on client error.

--- AllTrailsTests/RemotePlaceLoaderTests.swift
+++ AllTrailsTests/RemotePlaceLoaderTests.swift
@@ -10,13 +10,19 @@ import XCTest
 
 class RemotePlaceLoader: PlaceLoader {
     let client: HttpClientSpy
+    
+    enum Error: Swift.Error, Equatable {
+        case networkError
+    }
 
     init(client: HttpClientSpy) {
         self.client = client
     }
     
-    func load(with request: Request, completion: @escaping (Result<[Place], Error>) -> Void) {
-        client.request()
+    func load(with request: Request, completion: @escaping (Result<[Place], Swift.Error>) -> Void) {
+        client.request { _ in
+            completion(.failure(Error.networkError))
+        }
     }
 }
 
@@ -31,7 +37,6 @@ class RemotePlaceLoaderTests: XCTestCase {
     func test_load_requestsDataFromURL() {
         let (spy, loader) = makeSUT()
 
-        
         loader.load(with: anyRequest()) { _ in}
         
         XCTAssertEqual(spy.requests, 1)
@@ -40,11 +45,34 @@ class RemotePlaceLoaderTests: XCTestCase {
     func test_load_twice_requestsDataFromURLTwice() {
         let (spy, loader) = makeSUT()
 
-        
         loader.load(with: anyRequest()) { _ in}
         loader.load(with: anyRequest()) { _ in}
 
         XCTAssertEqual(spy.requests, 2)
+    }
+    
+    func test_load_returnsErrorOnHttpClientError() {
+        let (spy, loader) = makeSUT()
+
+        let exp = expectation(description: "wait on load")
+        var capturedError: Error?
+        
+        loader.load(with: anyRequest()) { result in
+            switch result {
+            case .success:
+                XCTFail("Expected a failure, received success instead")
+            case let .failure(error):
+                capturedError = error
+            }
+            exp.fulfill()
+        }
+        
+        let anyError = NSError(domain: "any-error", code: 0)
+        spy.completeWithError(anyError)
+
+        wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertEqual(capturedError as! RemotePlaceLoader.Error, RemotePlaceLoader.Error.networkError)
     }
     
     // MARK: - Helper Methods
@@ -59,13 +87,20 @@ class RemotePlaceLoaderTests: XCTestCase {
     private func anyRequest() -> Request {
         Request(keyword: nil, coordinates: LocationCoordinate(latitude: 0, longitude: 0), radius: 0, type: "a string")
     }
-
 }
 
 class HttpClientSpy {
-    var requests: Int = 0
+    var requests: Int {
+        completions.count
+    }
     
-    func request() {
-        requests = requests + 1
+    private var completions = [(Error) -> Void]()
+    
+    func request(completion: @escaping (Error) -> Void) {
+        completions.append(completion)
+    }
+    
+    func completeWithError(_ error: NSError, at index: Int = 0) {
+        completions[index](error)
     }
 }
