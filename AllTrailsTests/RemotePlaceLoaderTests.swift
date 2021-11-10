71ceb9c9511563f90b501177008213442407f7b9
Jose <daelhoser@gmail.com>
2021-11-10 06:02:37 -0800

    append 'Tests' to file and class name.

---
+++ AllTrailsTests/RemotePlaceLoaderTests.swift
@@ -0,0 +1,14 @@
+//
+//  RemotePlaceLoaderTests.swift
+//  RemotePlaceLoaderTests
+//
+//  Created by Jose Alvarez on 11/10/21.
+//
+
+import XCTest
+@testable import AllTrails
+
+
+class RemotePlaceLoaderTests: XCTestCase {
+
+}
