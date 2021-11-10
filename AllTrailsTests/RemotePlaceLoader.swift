ef7af357a777a0106b0596f2745ed53494e98685
Jose <daelhoser@gmail.com>
2021-11-10 05:58:32 -0800

    Rename test file and class to "RemotePlaceLoader"

---
+++ AllTrailsTests/RemotePlaceLoader.swift
@@ -0,0 +1,13 @@
+//
+//  RemotePlaceLoader.swift
+//  RemotePlaceLoader
+//
+//  Created by Jose Alvarez on 11/10/21.
+//
+
+import XCTest
+@testable import AllTrails
+
+class RemotePlaceLoader: XCTestCase {
+
+}
