a2129523b896e19e6c7f7be4b20773da569df11a
Jose <daelhoser@gmail.com>
2021-11-10 06:03:53 -0800

    On init loader does not request any Place data.

--- AllTrailsTests/RemotePlaceLoaderTests.swift
+++ AllTrailsTests/RemotePlaceLoaderTests.swift
@@ -8,7 +8,26 @@
 import XCTest
 @testable import AllTrails
 
+class RemotePlaceLoader {
+    let client: HttpClientSpy
+
+    init(client: HttpClientSpy) {
+        self.client = client
+    }
+}
 
 class RemotePlaceLoaderTests: XCTestCase {
 
+    func test_onInit_doesNotLoadPlaces() {
+        let spy = HttpClientSpy()
+        _ = RemotePlaceLoader(client: spy)
+        
+        XCTAssertEqual(spy.requests, 0)
+    }
+
+   
+}
+
+class HttpClientSpy {
+    var requests: Int = 0
 }
