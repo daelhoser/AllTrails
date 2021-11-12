59281835ccf7b6c9afd5b0e55819980aa03b34c0
Jose <daelhoser@gmail.com>
2021-11-12 12:46:01 -0800

    Create rootViewController in preparation of rendering and controlling map or list view.

---
+++ AllTrails/UIKit/RootView/RootViewController.swift
@@ -0,0 +1,28 @@
+//
+//  RootViewController.swift
+//  AllTrails
+//
+//  Created by Jose Alvarez on 11/12/21.
+//
+
+import UIKit
+
+final class RootViewController: UIViewController {
+    lazy var listViewController: PlacesTableViewController = {
+        let viewController = storyboard?.instantiateViewController(identifier: "PlacesTableViewController") as! PlacesTableViewController
+        
+        return viewController
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        addChild(listViewController)
+        view.addSubview(listViewController.view)
+        listViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        listViewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        listViewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        listViewController.view.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        listViewController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+}
