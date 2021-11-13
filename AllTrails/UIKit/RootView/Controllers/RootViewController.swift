710f1f0cc6f334ea314bc8e6783e8afdd060d539
Jose <daelhoser@gmail.com>
2021-11-13 07:18:27 -0800

    move RootViewComposer into its own class in its proper domain.

--- AllTrails/UIKit/RootView/Controllers/RootViewController.swift
+++ AllTrails/UIKit/RootView/Controllers/RootViewController.swift
@@ -7,19 +7,6 @@
 
 import UIKit
 
-final class RootViewComposer {
-    private init() {}
-    
-    static func compose(with loader: PlaceLoader, and dataLoader: DataLoader) -> RootViewController {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let rootViewController = storyboard.instantiateInitialViewController() as! RootViewController
-        rootViewController.loader = MainQueueDispatcherDecorator(decoratee: loader)
-        rootViewController.listViewController = PlaceViewComposer.compose(with: dataLoader)
-        
-        return rootViewController
-    }
-}
-
 final class RootViewController: UIViewController {
     @IBOutlet weak var topContainerView: UIView!
     @IBOutlet weak var textField: UITextField!
